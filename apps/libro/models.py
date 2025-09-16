9fdab0bc8a15081f9bf27611cc4db815e0014f0a
Elisa Mele <elisamele08@gmail.com>
2025-09-15 21:29:42 -0300

    Vistas y plantilla HTML para lista de libros

--- apps/libro/models.py
+++ apps/libro/models.py
@@ -2,12 +2,12 @@ from django.db import models
 from apps.autor.models import Autor
 
 class Libro(models.Model):
-    titulo = models.CharField(max_length=100)
-    descripcion = models.TextField(blank=True, null=True)
+    titulo = models.CharField(max_length=200)
+    descripcion = models.TextField(null=True, blank=True)
     fecha_publicacion = models.DateField()
     paginas = models.IntegerField()
     disponible = models.BooleanField(default=True)
-    autor = models.ForeignKey(Autor, on_delete=models.CASCADE, default=1)
+    autor = models.ForeignKey(Autor, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.titulo
--- apps/libro/views.py
+++ apps/libro/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Libro
 
-# Create your views here.
+def lista_libros(request):
+    libros = Libro.objects.all()
+    return render(request, "libro/lista_libros.html", {"libros": libros})
